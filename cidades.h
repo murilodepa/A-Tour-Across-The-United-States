5f79b183dbe877ab78cc1fc7e898f782aa74ec0f
Murilo Araujo <murilodepa@gmail.com>
2020-01-13 19:23:50 -0300

    adding cidades.h (matrix with all cities)

---
+++ cidades.h
@@ -0,0 +1,8 @@
+    char cidades[43][24] = {"0","seatle","portland","san francisco","los angeles",
+                            "san diego", "boise",  "lake tahoe ", "las vegas", "palm springs", "yellowstone",
+                            "salt lake city", "grand canyon", "phoenix",  "denver", "colorado springs",
+                            "vail","aspen", "albuquerque", "minneapolis",  "omaha",  "kansas city",  "dallas",
+                            "san antonio", "houston", "milwaukee", "chicago", "st louis", "little rock", "memphis",
+                            "nova orleans", "detroit", "indianapolis", "nashville", "atlanta", "orlando", "fort lauderdale",
+                            "miami",  "albany",  "boston", "nova york",  "washington",  "richmond"
+                           };
